1682ac660be9e3b98c618a7af7742bafdbad6f2e
raudbtw <105887271+raudbtw@users.noreply.github.com>
2022-07-01 18:05:53 +0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var sidenav = document.querySelectorAll(".sidenav");
+  var instances_sidenav = M.Sidenav.init(sidenav);
+
+  var modal = document.querySelectorAll(".modal");
+  var instances_modal = M.Modal.init(modal);
+});
