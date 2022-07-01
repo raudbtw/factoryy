ad7c6649136da70431d607a79981e60568aad4ef
raudbtw <105887271+raudbtw@users.noreply.github.com>
2022-07-01 18:01:45 +0300

    Delete main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var sidenav = document.querySelectorAll(".sidenav");
-  var instances_sidenav = M.Sidenav.init(sidenav);
-
-  var modal = document.querySelectorAll(".modal");
-  var instances_modal = M.Modal.init(modal);
-});
